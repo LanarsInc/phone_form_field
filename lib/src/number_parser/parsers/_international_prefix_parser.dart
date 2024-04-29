dbc93513ce7a658259431e95219537801328a2e6
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-04-29 13:44:30 +0200

    fix: phone number parser - handle omitted exit code correclty

--- lib/src/number_parser/parsers/_international_prefix_parser.dart
+++ lib/src/number_parser/parsers/_international_prefix_parser.dart
@@ -5,22 +5,25 @@ abstract class InternationalPrefixParser {
   ///
   ///  It expects a normalized [phoneNumber].
   ///  if phone starts with + it is removed
-  static String removeExitCode(
+  static (String, bool) removeExitCode(
     String phoneNumber, {
     PhoneMetadata? callerCountryMetadata,
     PhoneMetadata? destinationCountryMetadata,
   }) {
     if (phoneNumber.startsWith('+')) {
-      return phoneNumber.substring(1);
+      return (phoneNumber.substring(1), true);
     }
 
     // if the caller country was provided it's easy, just remove the exit code
     // from the phone number
     if (callerCountryMetadata != null) {
-      return _removeExitCodeWithMetadata(phoneNumber, callerCountryMetadata);
+      return (
+        _removeExitCodeWithMetadata(phoneNumber, callerCountryMetadata),
+        true
+      );
     }
 
-    return phoneNumber;
+    return (phoneNumber, false);
   }
 
   static String _removeExitCodeWithMetadata(
